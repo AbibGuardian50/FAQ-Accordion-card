e462fc1159573787f87f2d98c2b2bc2acbf7bcb5
Muhammad Abieb Basnuril <83066044+AbibGuardian50@users.noreply.github.com>
2022-12-24 02:14:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function showTeam() {
+    var team = document.getElementById("team");
+  if (team.style.display === "none") {
+    team.style.display = "block";
+  } else {
+    team.style.display = "none";
+  }
+}
+
+
+function showSize() {
+    var size = document.getElementById("size");
+  if (size.style.display === "none") {
+    size.style.display = "block";
+  } else {
+    size.style.display = "none";
+  }
+}
+
+function showReset() {
+    var reset = document.getElementById("reset");
+    if (reset.style.display === "none") {
+      reset.style.display = "block";
+    } else {
+      reset.style.display = "none";
+    }
+}
+
+function showCancel() {
+    var cancel = document.getElementById("cancel");
+    if (cancel.style.display === "none") {
+      cancel.style.display = "block";
+    } else {
+      cancel.style.display = "none";
+    }
+}
+
+function showSupport() {
+    var support = document.getElementById("support");
+    if (support.style.display === "none") {
+      support.style.display = "block";
+    } else {
+      support.style.display = "none";
+    }
+}
