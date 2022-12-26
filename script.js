565e12757be1e5cc5ffe21c446c0808506d23f32
Muhammad Abieb Basnuril <83066044+AbibGuardian50@users.noreply.github.com>
2022-12-27 00:33:55 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 function showTeam() {
     var team = document.getElementById("team");
-  if (team.style.display === "none") {
-    team.style.display = "block";
-  } else {
+  if (team.style.display === "block") {
     team.style.display = "none";
+  } else {
+    team.style.display = "block";
   }
 }
 
 
 function showSize() {
     var size = document.getElementById("size");
-  if (size.style.display === "none") {
-    size.style.display = "block";
-  } else {
+  if (size.style.display === "block") {
     size.style.display = "none";
+  } else {
+    size.style.display = "block";
   }
 }
 
 function showReset() {
     var reset = document.getElementById("reset");
-    if (reset.style.display === "none") {
-      reset.style.display = "block";
-    } else {
+    if (reset.style.display === "block") {
       reset.style.display = "none";
+    } else {
+      reset.style.display = "block";
     }
 }
 
 function showCancel() {
     var cancel = document.getElementById("cancel");
-    if (cancel.style.display === "none") {
-      cancel.style.display = "block";
-    } else {
+    if (cancel.style.display === "block") {
       cancel.style.display = "none";
+    } else {
+      cancel.style.display = "block";
     }
 }
 
 function showSupport() {
     var support = document.getElementById("support");
-    if (support.style.display === "none") {
-      support.style.display = "block";
-    } else {
+    if (support.style.display === "block") {
       support.style.display = "none";
+    } else {
+      support.style.display = "block";
     }
 }
